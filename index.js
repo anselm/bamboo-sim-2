f2431bcb822cb704a5af9436ba49f763ceec29b6
anselm <anselm@gmail.com>
2025-05-11 15:45:23 -0700

    use global

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-// import sys from 'https://cdn.jsdelivr.net/npm/orbital-sys@latest/src/sys.js'
+import sys from 'https://cdn.jsdelivr.net/npm/orbital-sys@1.1.5/src/sys.js'
 
-import sys from './shared/orbital-sys/src/sys.js'
+//import sys from './shared/orbital-sys/src/sys.js'
 
 sys({
 	load:[
-//		'https://cdn.jsdelivr.net/npm/orbital-volume@latest/volume.js',
-		'here/shared/orbital-volume/volume.js',
+		'https://cdn.jsdelivr.net/npm/orbital-volume@1.1.5/volume.js',
+//		'here/shared/orbital-volume/volume.js',
 		'here/agents/all.js'
 	]
 })
